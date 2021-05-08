3a6826ff7918643fd8838cf8c3f866aad939d089
“k-kaneko-Z7i” <tipapink0223@gmail.com>
2021-05-08 12:04:03 +0900

    課題再提出
    2-3

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -6,7 +6,7 @@ public class Task extends Calculator{
      * タスクの実行
      */
     public void doTask() {
-    	System.out.println("plusメソッドの引数が一つの場合:" + plus(10,1));
+    	System.out.println("plusメソッドの引数が一つの場合:" + plus(10));
     	System.out.println("plusメソッドの引数が二つの場合:" + plus(10,20));
     	System.out.println("plusメソッドの引数が三つの場合:"+ plus(10,20,30));
     	
