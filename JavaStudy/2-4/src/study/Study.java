7abf149379e78f7abd86478072e62711ae0ac32a
“k-kaneko-Z7i” <tipapink0223@gmail.com>
2021-05-08 12:06:44 +0900

    課題再提出
    2-4

--- JavaStudy/2-4/src/study/Study.java
+++ JavaStudy/2-4/src/study/Study.java
@@ -1,5 +1,11 @@
 package study;
 
+
+//①FruitsConstants.javaをインポートしなさい。
+import constants.FruitsConstants;
+/*ソース→ウィンドウの追加で自動削除を防ぐことが可能。
+ * 
+ */
 //②Fruits.javaをインポートしなさい。
 import fruits.Fruits;
 
@@ -10,15 +16,22 @@ import fruits.Fruits;
  * 全て回答し、課題にある画像と同じ表示になるようにしてください。
  *
  */
-public class Study {
+public class Study extends constants.FruitsConstants {
 
     public static void main(String[] args) {
-    	Fruits.printFruits("レモン",20);
     	
         /* ③ Fruits.javaのprintFruitsメソッドを呼び出しなさい。
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsに指定されている定数を設定してください。
          */
+    	Fruits.printFruits(FRUITS_LEMON_04,FRUITS_COUNT_20);
+    	
+    	/*extendsで継承しない場合
+    	 * Fruits.printFruits(fruits,count);
+    	 * String fruits = FruitsConstants.FRUITS_LEMON_04;
+    	 * int count =FruitsConstants.FRUITS_COUNT_20;
+    	 *でもOK
+    	 */
 
     }
 }
